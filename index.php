d0d764cb8f996b14c379c9a82ac827ccbca41a9a
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2020-02-08 09:47:29 -0500

    make it better

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title><h1>Receta de la natilla<h1></title>
+ </head>
+ <body>
+ <?php
+ echo '¡Hola ' . htmlspecialchars($_GET["name"]) . '!';
+ echo '<p>Query params and Heroku</p>'; ?>
+ </body>
+</html>
